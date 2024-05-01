e2dc274ae57a9ebf554554a87b35ca4589e041eb
Josh Rivera <beps@Mactop.local>
2024-04-30 20:35:28 -0500

    Completed first 2 algorithms

--- solution1.py
+++ solution1.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 1 below
+def iterate(num):
+    if num == 0:
+        return 0
+    else:
+        print(num)
+        iterate(num-1)
+
+n = 4
+iterate(n)
--- solution2.py
+++ solution2.py
@@ -1,1 +1,9 @@
 # Write code for algorithm 2 below
+def natural_numbers(x, i):
+    if x > i:
+        return
+    else:
+        print(x)
+        natural_numbers(x + 1, i)
+n=10
+print(natural_numbers(1, n))
